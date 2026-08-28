31e2bc62190246c22e7f5f262111b889b5a1bd1d
DaviMiguelReis <dmigueldosreis@gmail.com>
2026-08-28 15:34:26 -0300

    GerenciadorDeFilme

--- GerenciadorFilmes/ViewModels/FilmeFormularioViewModel.cs
+++ GerenciadorFilmes/ViewModels/FilmeFormularioViewModel.cs
@@ -15,12 +15,12 @@ namespace GerenciadorFilmes.ViewModels
         [Display(Name = "Duracao")]
         public int DuracaoMinutos { get; set; } 
 
-        [Range(1895, 2049, ErrorMessage = "O Ano está vazio.")]
+        [Range(1895, 2049, ErrorMessage = "Informe o Ano de lançamento.")]
         [Display(Name = "Ano lançamento")]
         public int AnoLancamento { get; set; }
 
-        [Required(ErrorMessage = "Selecione um Genero.")]
-        [Display(Name = "Genero")]
+        [Required(ErrorMessage = "Selecione um Gênero.")]
+        [Display(Name = "Gênero")]
         public int? GeneroId { get; set; }
 
         public List<SelectListItem> Generos { get; set; } = [];
